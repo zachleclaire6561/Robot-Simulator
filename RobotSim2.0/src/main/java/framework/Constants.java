70a38af99477e82c3658b1a0ec4bffc921051ef6
Zach LeClaire <zacharyjleclaire@gmail.com>
2020-06-13 23:28:59 -0700

    added rendering framework

---
+++ RobotSim2.0/src/main/java/framework/Constants.java
@@ -0,0 +1,10 @@
+package framework;
+
+public class Constants {
+	public final int scale = 1;
+	
+	/** The conversion factor from nano to base */
+	public static final double NANO_TO_BASE = 1.0e9;
+	
+	
+}
---
+++ RobotSim2.0/src/main/java/framework/RobotManual.java
@@ -0,0 +1,16 @@
+package framework;
+
+public class RobotManual extends SimulationFrame {
+
+	public RobotManual(String name, double scale) {
+		super("RobotManual", scale);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initializeWorld() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
